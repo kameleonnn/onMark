60d0156f3e400733ac5f738bd9dfe179e2e9d83c
kameleonnn <eryk.kozub@op.pl>
2026-05-19 18:39:17 +0200

    added checking if file is saved, 'global' error function

--- src/main/java/dev/kameleonnn/onmark/App.java
+++ src/main/java/dev/kameleonnn/onmark/App.java
@@ -1,10 +1,5 @@
 package dev.kameleonnn.onmark;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static java.lang.IO.print;
-import java.util.Scanner;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -23,36 +18,34 @@ import javafx.stage.WindowEvent;
  * JavaFX App
  */
 public class App extends Application {
-    private static final Logger LOGGER=Logger.getLogger(App.class.getName());
-    static Scanner input = new Scanner(System.in);
     static String fxml = "MainWin.fxml";
-    private static Scene scene;
+    public static Scene scene;
+    public static String filename = "";
     public static String data = "";
-
+    public static boolean saved = true;
+    public static String[] recent = new String[6];
 
     /**
      * Main function
      * @param args arguments form terminal/command line execution.
      * @throws IOException from function loadFXML()
      */
-
-    public static void main(String[] args) throws IOException{
-        scene = new Scene(loadFXML(fxml), 600, 750);
+    public static void main(String[] args) throws IOException {
+        scene = new Scene(loadFXML(fxml));
         launch();
     }
+
     /**
      * Closes the program upon user request
      */
-    public static void close(){
+    public static void close() {
         System.exit(0);
     }
-    
+
     /**
-     * Implementtion of start() function from JavaFX. 
-     * Responsible for starting the GUI
+     * Implementation of start() function from JavaFX. Responsible for starting the GUI
      * @param stage JavaFX Stage class
-     * @throws IOException from setRoot(string)
-     * @throws IOException from initDatabase()
+     * @throws IOException
      */
     @Override
     public void start(Stage stage) throws IOException {
@@ -60,33 +53,16 @@ public class App extends Application {
         stage.setScene(scene);
         stage.setTitle("onMark");
         stage.show();
-        
         stage.setOnCloseRequest((WindowEvent event) -> {
-	    if(FileOp.getInstance().isSaved()==false){
-		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setContentText(Strings.FILE_EXIT_SAVE_PROMPT.text);
-                Optional<ButtonType> option = alert.showAndWait();
-                if(option.get()==ButtonType.YES){
-                    try {
-                        FileOp.getInstance().save(data);
-                    } catch (IOException ex) {
-                        Alert error = new Alert(Alert.AlertType.ERROR);
-                        error.setContentText(Strings.FILE_SAVE_ERROR.text);
-                        error.show();
-                    }
-                }
-                if(option.get()==ButtonType.NO){
-                    Platform.exit();
-                }
-	    }
-	});
-        
+            saveCheck();
+            if (saved) { Platform.exit(); }
+        });
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-    
+
     /**
      * Loads contents of main window from .fxml file
      * @param fxml URL to .fxml file
@@ -98,4 +74,35 @@ public class App extends Application {
         return fxmlLoader.load();
     }
 
+    public static void saveCheck() {
+        if (!saved) {
+            Alert savePrompt = new Alert(Alert.AlertType.CONFIRMATION);
+            savePrompt.setContentText(Strings.FILE_EXIT_SAVE_PROMPT.text);
+            savePrompt.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
+            Optional<ButtonType> option = savePrompt.showAndWait();
+            if (option.get() == ButtonType.YES) {
+                try {
+                    Files.save(App.data, App.filename);
+                    Files.closeFile();
+
+                } catch (IOException ex) {
+                    Alert error = new Alert(Alert.AlertType.ERROR);
+                    error.setContentText(Strings.FILE_SAVE_ERROR.text);
+                    error.show();
+                    saved = false;
+                }
+            }
+            if (option.get() == ButtonType.NO) {
+                Files.closeFile();
+                saved = true;
+            }
+            if(option.get()==ButtonType.CANCEL){ saved = false;}
+        }
+    }
+    
+    public static void errorAlert(String msg){
+        Alert error = new Alert(Alert.AlertType.ERROR);
+        error.setContentText(msg);
+        error.show();
+    }
 }
