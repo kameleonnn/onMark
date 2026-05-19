ffaa4502b3d04e0bc1d9746bedbfdbea2bac809c
kameleonnn <kameleonnn@codeberg.org>
2026-05-19 11:41:39 +0200

    add file handling methods, minor adjustments in App class, update project config

---
+++ src/main/java/dev/kameleonnn/onmark/Files.java
@@ -0,0 +1,81 @@
+package dev.kameleonnn.onmark;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author kameleonnn
+ */
+public class Files {
+    
+    /**
+     * Creates new file from default values
+     * @param filename string
+     * @return 'true' if file was created successfully, 'false' on failure
+     * @throws IOException if file stream cannot be created
+     */
+    public boolean createFile(String filename) throws IOException{
+        return new File(filename).createNewFile();
+    }
+    
+    /**
+     * Clear contents of given file
+     * @param filename string
+     * @throws IOException
+     */
+    public static void clearFile(String filename) throws IOException{
+	new BufferedWriter(new FileWriter(filename, false)).close();
+    } 
+    
+    /**
+     * Reads contents of file line after line and generates database contents ('Database.task')
+     * @param filename
+     * @throws IOException if file cannot be read
+     */
+    public static void readFile(String filename) throws IOException{
+        try (BufferedReader dataRead = checkIfFileCanOpen(filename)) {
+            String line;
+            while((line=dataRead.readLine())!=null){
+                if(!line.equals("")){
+                    App.data.concat(line);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Saves contents of given string to file with given filename line by line
+     * @param data string to be written to file
+     * @param filename file to be written to
+     * @return true after completing method
+     * @throws IOException
+     */
+    public static boolean save(String data, String filename) throws IOException{
+	clearFile(filename);
+        try (BufferedWriter dataWrite = new BufferedWriter(new FileWriter(filename,false))) {
+            for(int i=0;i<data.length();i++){
+                if(!data.equals("")){
+                    dataWrite.write(data);
+                    dataWrite.flush();
+                }
+            }
+            return true;
+        }
+    }  
+    
+    /**
+     * checks if file with given filename can be opened
+     * @param filename string
+     * @return BufferedReader on success
+     * @throws IOException
+     */
+    public static BufferedReader checkIfFileCanOpen(String filename) throws IOException{
+        return new BufferedReader(new FileReader(filename)); 
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,10 @@ module dev.kameleonnn.onmark {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.logging;
+    requires java.base;
 
     opens dev.kameleonnn.onmark to javafx.fxml;
+    opens dev.kameleonnn.onmark.controller;
     exports dev.kameleonnn.onmark;
+    exports dev.kameleonnn.onmark.controller;
 }
