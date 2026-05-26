c0fb1c1dcd2d641a5d7acada61b0908aba383957
kameleonnn <eryk.kozub@op.pl>
2026-05-26 15:07:07 +0200

    added functionality to buttonFileSave, buttonFileNew, buttonFileOpen

--- src/main/java/dev/kameleonnn/onmark/controller/EditorController.java
+++ src/main/java/dev/kameleonnn/onmark/controller/EditorController.java
@@ -1,10 +1,14 @@
 package dev.kameleonnn.onmark.controller;
 
 import dev.kameleonnn.onmark.App;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.SplitPane;
 import javafx.scene.control.TextArea;
@@ -28,6 +32,18 @@ public class EditorController implements Initializable {
     private TextArea textInput;
     @FXML
     private ScrollPane splitPreview;
+    @FXML
+    private Button buttonFileSave;
+    @FXML
+    private Button buttonFileNew;
+    @FXML
+    private Button buttonFileOpen;
+    @FXML
+    private Button buttonEditBold;
+    @FXML
+    private Button buttonEditItalic;
+    
+    private MainWinController main;
 
     /**
      * Initializes the controller class.
@@ -35,8 +51,32 @@ public class EditorController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+        buttonFileSave.setOnAction((new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                main.saveFile();
+            }
+        }));
+        
+        buttonFileNew.setOnAction((new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                main.newFile();
+            }
+        }));
+        
+        buttonFileOpen.setOnAction((new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                main.openFile();
+            }
+        }));
         
     } 
+    
+    public void getMain(MainWinController controller){
+        main = controller;
+    }
     
     public void setupRespSize(AnchorPane mainCont){
         editorMainCont.prefWidthProperty().bind(mainCont.widthProperty());
